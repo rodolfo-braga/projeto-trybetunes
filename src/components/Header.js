d7cdb53622155a4d13dba30aca58540373a2c3c0
Rodolfo Braga <rodolfo.braga@yahoo.com.br>
2021-09-14 22:30:32 -0300

    Requisito 4

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import { getUser } from '../services/userAPI';
 import Loading from './Loading';
 
@@ -37,6 +38,26 @@ class Header extends Component {
             <div>
               <h1>Header</h1>
               <h3 data-testid="header-user-name">{ `Welcome back ${userName}` }</h3>
+              <nav>
+                <Link
+                  to="/search"
+                  data-testid="link-to-search"
+                >
+                  Pesquisa
+                </Link>
+                <Link
+                  to="/favorites"
+                  data-testid="link-to-favorites"
+                >
+                  Favoritas
+                </Link>
+                <Link
+                  to="/profile"
+                  data-testid="link-to-profile"
+                >
+                  Perfil
+                </Link>
+              </nav>
             </div>
           )}
       </header>
